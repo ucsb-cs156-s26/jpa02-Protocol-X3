65697472d64ae098a085691741cb51009789f678
Whisper <qxia@ucsb.edu>
2026-04-07 17:58:27 -0700

    Update Team test

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +22,50 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_correct_value() {
+        //case 1: same object (obj == this)
+        Team other = team;
+        assertEquals(true, team.equals(other));
+
+        //case 2: different object
+        Object notATeam = new Object();
+        assertEquals(false, team.equals(notATeam));
+
+        //case 3-1: same name same members
+        other = new Team("test-team");
+        assertEquals(true, team.equals(other));
+
+        //case 3-2: different name same members
+        other = new Team("other-team");
+        assertEquals(false, team.equals(other));
+
+        //case 3-3: same name different members
+        other = new Team("test-team");
+        other.setMembers(new ArrayList<>(Arrays.asList("member1")));
+        assertEquals(false, team.equals(other));
+    }
+
+    @Test
+    public void hashCode_returns_correct_value() {
+        //case 1: same object (obj == this)
+        Team other = team;
+        assertEquals(true, team.hashCode() == other.hashCode());
+
+        //case 2-1: same name same members
+        assertEquals(true, team.hashCode() == new Team("test-team").hashCode());
+
+        //case 2-2: different name
+        assertEquals(false, team.hashCode() == new Team("other-team").hashCode());
+
+        //case 2-3: same name different members
+        other = new Team("test-team");
+        other.setMembers(new ArrayList<>(Arrays.asList("member1")));
+        assertEquals(false, team.hashCode() == other.hashCode());
+    }
 }
